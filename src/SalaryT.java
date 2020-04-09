b659adbfbdced1347f2fe33566ef13df216f29ab
Jonathan Cels <celsj@mcmaster.ca>
2020-04-09 18:30:27 -0400

    SalaryT constructor and fields

--- src/SalaryT.java
+++ src/SalaryT.java
@@ -1,4 +1,20 @@
 
 public class SalaryT {
-    
+    private String name; //Format as lastname, firstname
+    private String pos;
+    private String employer;
+    private String sect;
+    private String city;
+    private int sal;
+    private int year;
+
+    public SalaryT(String name, int salary, String sector, String employer, String position, String city, int year){
+        this.name = name;
+        this.sal = salary;
+        this.sect = sector;
+        this.employer = employer;
+        this.pos = position;
+        this.city = city;
+        this.year = year;
+    }
 }
