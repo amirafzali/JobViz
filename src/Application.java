deb8a9d0f714e06df54c36864075ac0c2b6b32f5
amirafzali <amir@afzali.ca>
2020-04-10 01:57:56 -0400

    Add app main class

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,17 @@
 package src;
 
 public class Application {
+    public static void main(String[] args) {
+        readTest();
+    }
+
+    public static void readTest() {
+        try {
+            ParseT parse2019 = new ParseT("data/data.csv");
+            System.out.println(parse2019.getAllSalaries().size());
+            System.out.println(parse2019.getAllSalaries().getSalaries().get(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
