1795234afd26aa07a643b242acda9df4431ef4dc
amirafzali <amir@afzali.ca>
2020-04-10 00:07:46 -0400

    Slight modifications to SalaryT

--- src/SalaryT.java
+++ src/SalaryT.java
@@ -1,17 +1,19 @@
 package src;
 public class SalaryT {
-    private String name; //Format as lastname, firstname
-    private String pos;
-    private String employer;
-    private String sect;
-    private String city;
-    private int sal;
-    private int year;
+    private final String name;
+    private final String pos;
+    private final String employer;
+    private final String sector;
+    private final String city;
+    private final double salary;
+    private final double benefits;
+    private final int year;
 
-    public SalaryT(String name, int salary, String sector, String employer, String position, String city, int year){
+    public SalaryT(String sector, String name, double salary, double benefits, String employer, String position, int year, String city){
         this.name = name;
-        this.sal = salary;
-        this.sect = sector;
+        this.salary = salary;
+        this.benefits = benefits;
+        this.sector = sector;
         this.employer = employer;
         this.pos = position;
         this.city = city;
@@ -31,19 +33,23 @@ public class SalaryT {
     }
 
     public String getSector(){
-        return this.sect;
+        return this.sector;
     }
 
     public String getCity(){
         return this.city;
     }
 
-    public int getSalary(){
-        return this.sal;
+    public double getSalary(){
+        return this.salary;
     }
 
     public int getYear(){
         return this.year;
+    }
+
+    public double getBenefits() {
+        return benefits;
     }
 
     /**
@@ -51,7 +57,7 @@ public class SalaryT {
      */
     @Override
     public boolean equals(Object object){
-        if (this == object) 
+        if (this == object)
             return true;
 
         if (object == null || this.getClass() != object.getClass())
@@ -62,20 +68,20 @@ public class SalaryT {
         return (this.name.contentEquals(that.getName()) &&
                 this.pos.contentEquals(that.getPosition()) &&
                 this.employer.contentEquals(that.getEmployer()) &&
-                this.sect.contentEquals(that.getSector()) &&
+                this.sector.contentEquals(that.getSector()) &&
                 this.city.contentEquals(that.getCity()) &&
-                this.sal == that.getSalary() &&
+                this.salary == that.getSalary() &&
                 this.year == that.getYear());
     }
 
     /**
-     * Computes a hashCode for a SalaryT object. 
+     * Computes a hashCode for a SalaryT object.
      * A collision will occur if two people have the same name, employer and salary.
      */
     @Override
     public int hashCode(){
         int result = this.name.hashCode();
-        result *= 31 * (int) (this.sal ^ (this.sal >>> 32));
+        result *= 31 * (int) (this.salary ^ (this.salary >>> 32));
         result *= 31 * this.employer.hashCode();
         return result;
     }
