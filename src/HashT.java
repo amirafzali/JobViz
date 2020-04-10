7e9af5b9fe278f44fb6c009c154a951cd5c1e1c8
Rupinder Nagra <rupinder0410@gmail.com>
2020-04-09 20:31:08 -0400

    added HashT skeleton

--- src/HashT.java
+++ src/HashT.java
@@ -1,7 +1,26 @@
-class HashT {
+package src;
+
+class HashT<T> {
     private SalaryT[][] h;
-    
+
     public HashT(SalaryT[] salaries){
 
     }
+
+    // public SalaryT[] get(T t) {
+
+    // }
+
+    public void add(SalaryT s) {
+
+    }
+
+    public void remove(T s){
+
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
 }
