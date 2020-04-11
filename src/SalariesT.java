3cef07d70cf58d84fae30f87b94c92e36255d466
Jarrod Colwell <jarrodcolwell@Jarrods-MacBook-Pro.local>
2020-04-11 19:33:34 -0400

    Changed method name and added some Javadoc

--- src/SalariesT.java
+++ src/SalariesT.java
@@ -3,139 +3,230 @@ package src;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class SalariesT extends SortT{
-    private final ArrayList<SalaryT> s;
-    private final int year;
-    private final String[] sectors, employers, positions;
+public class SalariesT extends SortT {
+	private final ArrayList<SalaryT> s;
+	private final int year;
+	private final String[] sectors, employers, positions;
 
-    public SalariesT(ArrayList<SalaryT> salaries, int year, String[] sectors, String[] employers, String[] positions) {
-        this.s = salaries;
-        this.year = year;
-        this.sectors = sectors;
-        this.employers = employers;
-        this.positions = positions;
-        Arrays.sort(this.sectors);
-        Arrays.sort(this.employers);
-        Arrays.sort(this.positions);
-    }
+	/**
+	 * @param salaries
+	 * @param year
+	 * @param sectors
+	 * @param employers
+	 * @param positions
+	 */
+	public SalariesT(ArrayList<SalaryT> salaries, int year, String[] sectors, String[] employers, String[] positions) {
+		this.s = salaries;
+		this.year = year;
+		this.sectors = sectors;
+		this.employers = employers;
+		this.positions = positions;
+		Arrays.sort(this.sectors);
+		Arrays.sort(this.employers);
+		Arrays.sort(this.positions);
+	}
 
-    public SalariesT(ArrayList<SalaryT> salaries, int year) {
-        this.s = salaries;
-        this.year = year;
-        this.sectors = null;
-        this.employers = null;
-        this.positions = null;
-    }
+	/**
+	 * @param salaries
+	 * @param year
+	 */
+	public SalariesT(ArrayList<SalaryT> salaries, int year) {
+		this.s = salaries;
+		this.year = year;
+		this.sectors = null;
+		this.employers = null;
+		this.positions = null;
+	}
 
-    public int size() {
-        return this.getSalaries().size();
-    }
+	/**
+	 * Size method for SalariesT
+	 * 
+	 * @return int The number of salaries
+	 */
+	public int size() {
+		return this.getSalaries().size();
+	}
 
-    public ArrayList<SalaryT> getSalaries() {
-        return this.s;
-    }
+	/**
+	 * Get method for salaries
+	 * 
+	 * @return ArrayList<SalaryT> The list of salaries
+	 */
+	public ArrayList<SalaryT> getSalaries() {
+		return this.s;
+	}
 
-    public SalaryT getSalary(String name) {
-        for (SalaryT e: this.getSalaries()) {
-            if (e.getName().equals(name)) {
-                return e;
-            }
-        }
-        return null;
-    }
+	/**
+	 * Get method for an individual's Salary
+	 * 
+	 * @param name The name of the individual who's salary is being retrieved
+	 * @return SalaryT The individual's SalaryT object if it exists, null otherwise
+	 */
+	public SalaryT getSalary(String name) {
+		for (SalaryT e : this.getSalaries()) {
+			if (e.getName().equals(name)) {
+				return e;
+			}
+		}
+		return null;
+	}
 
-    public void add(SalaryT salary) {
-        this.getSalaries().add(salary);
-    }
+	/**
+	 * Method that adds a salary to the list of salaries
+	 * 
+	 * @param salary The salary to be added
+	 */
+	public void add(SalaryT salary) {
+		this.getSalaries().add(salary);
+	}
 
-    public void sortSalary(boolean ascending) {
-        mergeSort(this.getSalaries(),0, this.getSalaries().size() - 1, ascending, new sortBySalary());
-    }
+	/**
+	 * The public interface for sorting salary
+	 * 
+	 * @param ascending A boolean representing sort order
+	 */
+	public void sortSalary(boolean ascending) {
+		mergeSort(this.getSalaries(), 0, this.getSalaries().size() - 1, ascending, new sortBySalary());
+	}
 
-    public void sortPosition(boolean ascending) {
-        mergeSort(this.getSalaries(),0, this.getSalaries().size() - 1, ascending, new sortByPosition());
-    }
+	/**
+	 * The public interface for sorting position
+	 * 
+	 * @param ascending A boolean representing sort order
+	 */
+	public void sortPosition(boolean ascending) {
+		mergeSort(this.getSalaries(), 0, this.getSalaries().size() - 1, ascending, new sortByPosition());
+	}
 
-    public void sortSector(boolean ascending) {
-        mergeSort(this.getSalaries(),0, this.getSalaries().size() - 1, ascending, new sortBySector());
-    }
+	/**
+	 * The public interface for sorting sector
+	 * 
+	 * @param ascending A boolean representing sort order
+	 */
+	public void sortSector(boolean ascending) {
+		mergeSort(this.getSalaries(), 0, this.getSalaries().size() - 1, ascending, new sortBySector());
+	}
 
-    public void sortEmployer(boolean ascending) {
-        mergeSort(this.getSalaries(),0, this.getSalaries().size() - 1, ascending, new sortByEmployer());
-    }
+	/**
+	 * The public interface for sorting employer
+	 * 
+	 * @param ascending A boolean representing sort order
+	 */
+	public void sortEmployer(boolean ascending) {
+		mergeSort(this.getSalaries(), 0, this.getSalaries().size() - 1, ascending, new sortByEmployer());
+	}
 
-    public boolean isSorted() {
-        for (int i = 1; i < this.size(); i++) {
-            if (this.getSalaries().get(i - 1).getSalary() > this.getSalaries().get(i).getSalary()) {
-                return false;
-            }
-        }
-        return true;
-    }
+	/**
+	 * Method to check if the salaries are sorted in ascending order
+	 * 
+	 * @return boolean True if the salaries are sorted, false otherwise
+	 */
+	public boolean isSorted() {
+		for (int i = 1; i < this.size(); i++) {
+			if (this.getSalaries().get(i - 1).getSalary() > this.getSalaries().get(i).getSalary()) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    public SalariesT copy() {
-        return new SalariesT(new ArrayList<SalaryT>(this.getSalaries()), this.year);
-    }
+	/**
+	 * Method to copy a SalariesT object into another
+	 * 
+	 * @return SalariesT New SalariesT with copied values
+	 */
+	public SalariesT copy() {
+		return new SalariesT(new ArrayList<SalaryT>(this.getSalaries()), this.year);
+	}
 
-    public SalariesT filterSalary(int low, int high) {
-        SalariesT salaries = new SalariesT(new ArrayList<SalaryT>(), this.year);
-        for (SalaryT e: this.getSalaries()) {
-            if (low < e.getSalary() && e.getSalary() < high) {
-                salaries.add(e);
-            }
-        }
-        return salaries;
-    }
+	/**
+	 * Creates a new Salaries object and adds only those with salaries between the
+	 * upper and lower bounds
+	 * 
+	 * @param low  The lower bound on salary
+	 * @param high The upper bound on salary
+	 * @return SalariesT All of the salaries that fall between the lower and upper
+	 *         bounds
+	 */
+	public SalariesT filterSalary(int low, int high) {
+		SalariesT salaries = new SalariesT(new ArrayList<SalaryT>(), this.year);
+		for (SalaryT e : this.getSalaries()) {
+			if (low < e.getSalary() && e.getSalary() < high) {
+				salaries.add(e);
+			}
+		}
+		return salaries;
+	}
 
-    public SalariesT filterSector(String name) {
-        SalariesT salaries = new SalariesT(new ArrayList<SalaryT>(), this.year);
-        for (SalaryT e: this.getSalaries()) {
-            if (e.getSector().equals(name)) {
-                salaries.add(e);
-            }
-        }
-        return salaries;
-    }
+	/**
+	 * Creates a new Salaries object and adds only those that have the same sector
+	 * as the parameter
+	 * 
+	 * @param sector The name of the desired sector
+	 * @return SalariesT All of the salaries that are in the desired sector
+	 */
+	public SalariesT filterSector(String sector) {
+		SalariesT salaries = new SalariesT(new ArrayList<SalaryT>(), this.year);
+		for (SalaryT e : this.getSalaries()) {
+			if (e.getSector().equals(sector)) {
+				salaries.add(e);
+			}
+		}
+		return salaries;
+	}
 
-    public SalariesT filterEmployer(String name) {
-        SalariesT salaries = new SalariesT(new ArrayList<SalaryT>(), this.year);
-        for (SalaryT e: this.getSalaries()) {
-            if (e.getEmployer().equals(name)) {
-                salaries.add(e);
-            }
-        }
-        return salaries;
-    }
+	/**
+	 * Creates a new Salaries object and adds only those that have the same employer
+	 * as the parameter
+	 * 
+	 * @param employer The name of the desired employer
+	 * @return SalariesT All of the salaries with the desired employer
+	 */
+	public SalariesT filterEmployer(String employer) {
+		SalariesT salaries = new SalariesT(new ArrayList<SalaryT>(), this.year);
+		for (SalaryT e : this.getSalaries()) {
+			if (e.getEmployer().equals(employer)) {
+				salaries.add(e);
+			}
+		}
+		return salaries;
+	}
 
-    public SalariesT filterPosition(String name) {
-        SalariesT salaries = new SalariesT(new ArrayList<SalaryT>(), this.year);
-        for (SalaryT e: this.getSalaries()) {
-            if (e.getPosition().equals(name)) {
-                salaries.add(e);
-            }
-        }
-        return salaries;
-    }
+	/**
+	 * Creates a new Salaries object and adds only those that have the same position
+	 * as the parameter
+	 * 
+	 * @param position The name of the desired position
+	 * @return SalariesT All of the salaries with the desired position
+	 */
+	public SalariesT filterPosition(String position) {
+		SalariesT salaries = new SalariesT(new ArrayList<SalaryT>(), this.year);
+		for (SalaryT e : this.getSalaries()) {
+			if (e.getPosition().equals(position)) {
+				salaries.add(e);
+			}
+		}
+		return salaries;
+	}
 
-    public double positionMedian(String position) {
-        double median = 0.0;
-        for (SalaryT e: this.filterPosition(position).getSalaries()) {
-            median += e.getSalary();
-        }
-        median = median/this.filterPosition(position).getSalaries().size();
-        return median;
-    }
+	public double positionMean(String position) {
+		double median = 0.0;
+		for (SalaryT e : this.filterPosition(position).getSalaries()) {
+			median += e.getSalary();
+		}
+		median = median / this.filterPosition(position).getSalaries().size();
+		return median;
+	}
 
-    public String[] getSectors() {
-        return sectors;
-    }
+	public String[] getSectors() {
+		return sectors;
+	}
 
-    public String[] getEmployers() {
-        return employers;
-    }
+	public String[] getEmployers() {
+		return employers;
+	}
 
-    public String[] getPositions() {
-        return positions;
-    }
+	public String[] getPositions() {
+		return positions;
+	}
 }
-
