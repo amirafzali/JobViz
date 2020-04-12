53d36bc59c0926f6a8b287be11b5615795194526
Jarrod Colwell <jarrodcolwell@Jarrods-MacBook-Pro.local>
2020-04-12 15:54:14 -0400

    Added Javadoc

--- src/Application.java
+++ src/Application.java
@@ -1,36 +1,44 @@
 package src;
 
-import src.panels.AppFrame;
-
 import java.util.ArrayList;
 
+import src.panels.AppFrame;
+
+/**
+ * Main runner class
+ *
+ */
 public class Application {
-    public static void main(String[] args) {
-        launch();
-    }
 
-    private static void launch() {
-        try {
+	public static void main(String[] args) {
+		launch();
+	}
 
-            ParseT parse2019 = new ParseT("data/data2019.csv");
-            ParseT parse2018 = new ParseT("data/data2018.csv");
-            ParseT parse2017 = new ParseT("data/data2017.csv");
+	/**
+	 * Parses the input data and launches the GUI
+	 */
+	private static void launch() {
+		try {
 
-            ArrayList<SalariesT> all = new ArrayList<>();
-            all.add(parse2017.getAllSalaries());
-            all.add(parse2018.getAllSalaries());
-            all.add(parse2019.getAllSalaries());
+			ParseT parse2019 = new ParseT("data/data2019.csv");
+			ParseT parse2018 = new ParseT("data/data2018.csv");
+			ParseT parse2017 = new ParseT("data/data2017.csv");
 
-            System.out.println("Launching app... please wait :)");
-            System.out.println("2019 rows: "+parse2019.getAllSalaries().size());
-            System.out.println("2018 rows: "+parse2018.getAllSalaries().size());
-            System.out.println("2017 rows: "+parse2017.getAllSalaries().size());
-            System.out.println("Almost done loading...");
-            new AppFrame(parse2019.getAllSalaries(), all);
-            System.out.println("Done!");
+			ArrayList<SalariesT> all = new ArrayList<>();
+			all.add(parse2017.getAllSalaries());
+			all.add(parse2018.getAllSalaries());
+			all.add(parse2019.getAllSalaries());
 
-        } catch (Exception e) {
-            System.out.println("App launch failed! Probably due to missing CSVs.");
-        }
-    }
+			System.out.println("Launching app... please wait :)");
+			System.out.println("2019 rows: " + parse2019.getAllSalaries().size());
+			System.out.println("2018 rows: " + parse2018.getAllSalaries().size());
+			System.out.println("2017 rows: " + parse2017.getAllSalaries().size());
+			System.out.println("Almost done loading...");
+			new AppFrame(parse2019.getAllSalaries(), all);
+			System.out.println("Done!");
+
+		} catch (Exception e) {
+			System.out.println("App launch failed! Probably due to missing CSVs.");
+		}
+	}
 }
